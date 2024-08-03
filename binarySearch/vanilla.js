65eaa10a17c4f61be9f9b825b3aae9071290cb69
Haolin Wu <haolin@haolin.dev>
2024-08-03 13:54:11 +0800

    Add vanilla binary search - O(log(n))

---
+++ binarySearch/vanilla.js
@@ -0,0 +1,22 @@
+const input = [1, 4, 5, 7, 9, 12, 15, 18, 19, 22, 25, 29, 40, 50];
+
+const vanillaBinarySearch = (arr, target) => {
+    // Initialise pointers to start and end.
+    let left = 0;
+    let right = arr.length - 1;
+    while (left !== right) {
+        const midPoint =  Math.floor((left + right) / 2);
+        if (arr[midPoint] > target) {
+            right = midPoint - 1;
+        } else if (arr[midPoint] < target) {
+            left = midPoint + 1;
+        } else {
+            return midPoint;
+        }
+    }
+    return arr[left] === target ? left : undefined;
+}
+
+const index = vanillaBinarySearch(input, 12);
+
+console.log(index);
